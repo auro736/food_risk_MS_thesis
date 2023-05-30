5c8e28a2b073234425ec0ddab612c0ef52b5e759
auro736 <auroragensale@gmail.com>
2023-05-30 11:04:57 +0000

    error analysis wip

--- tasks/prova.py
+++ tasks/prova.py
@@ -16,6 +16,15 @@ ASSIGN_WEIGHT = True
 SEED = 42
 
 # TEST ACC = 0.837 (20 EPOCHE) QUANDO FACCIO TRAIN-VAL E TEST
+def load_local_model(model_path, config_path, device, model_name):
+
+    config = config = AutoConfig.from_pretrained(config_path)
+
+    model = ModelForWeightedSequenceClassification(model_name=model_name,config=config)
+    checkpoint = torch.load(model_path, map_location=device)
+    model.load_state_dict(checkpoint)
+
+    return model
 
 def main():
 
@@ -27,57 +36,53 @@ def main():
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    saved_model_path = '/home/cc/rora_tesi_new/log/log_TRC/roberta-large/bertweet-seq/20_epoch/data/True_weight/42_seed/saved-model/pytorch_model.bin'
+    model_path = '/home/cc/rora_tesi_new/log/log_TRC/roberta-large/bertweet-seq/20_epoch/data/True_weight/42_seed/saved-model/pytorch_model.bin'
+    model_name = model_path.split('/')[6]
+    
     config_path = '/home/cc/rora_tesi_new/log/log_TRC/roberta-large/bertweet-seq/20_epoch/data/True_weight/42_seed/saved-model/config.json'
 
-    config = AutoConfig.from_pretrained(config_path)
+    load_local_model(model_path, config_path, device, model_name)
 
-    model = ModelForWeightedSequenceClassification(model_name='roberta-large',config=config)
 
-    checkpoint = torch.load(saved_model_path, map_location=device)
 
-    # print(checkpoint.keys())
+    # model = model.to(device)
 
-    model.load_state_dict(checkpoint)
+    # train_data_path = '/home/cc/rora_tesi_new/data/train.p'
+    # val_data_path = '/home/cc/rora_tesi_new/data/dev.p'
+    # test_data_path = '/home/cc/rora_tesi_new/data/test.p'
 
-    model = model.to(device)
+    # train_data = pd.read_pickle(train_data_path)
+    # val_data = pd.read_pickle(val_data_path)
+    # test_data = pd.read_pickle(test_data_path)
 
-    train_data_path = '/home/cc/rora_tesi_new/data/train.p'
-    val_data_path = '/home/cc/rora_tesi_new/data/dev.p'
-    test_data_path = '/home/cc/rora_tesi_new/data/test.p'
+    # need_columns = ['tweet_tokens', 'sentence_class']
 
-    train_data = pd.read_pickle(train_data_path)
-    val_data = pd.read_pickle(val_data_path)
-    test_data = pd.read_pickle(test_data_path)
+    # _, Y_train = extract_from_dataframe(train_data, need_columns)
+    # X_dev_raw, Y_dev = extract_from_dataframe(val_data, need_columns)
+    # X_test_raw, Y_test = extract_from_dataframe(test_data, need_columns)
 
-    need_columns = ['tweet_tokens', 'sentence_class']
+    # eval_batch_size = Y_dev.shape[0]
+    # test_batch_size = Y_test.shape[0]
 
-    _, Y_train = extract_from_dataframe(train_data, need_columns)
-    X_dev_raw, Y_dev = extract_from_dataframe(val_data, need_columns)
-    X_test_raw, Y_test = extract_from_dataframe(test_data, need_columns)
+    # # weight of each class in loss function
+    # class_weight = None
+    # if ASSIGN_WEIGHT:
+    #     class_weight = [np.array(Y_train).shape[0] / (np.array(Y_train) == i).sum() for i in range(len(set(Y_train)))]
+    #     class_weight = torch.FloatTensor(class_weight)
 
-    eval_batch_size = Y_dev.shape[0]
-    test_batch_size = Y_test.shape[0]
+    # tokenizer = AutoTokenizer.from_pretrained(BERT_MODEL, normalization=True)
+    # X_test, masks_test = tokenize_with_new_mask(X_test_raw, MAX_LENGTH, tokenizer)
 
-    # weight of each class in loss function
-    class_weight = None
-    if ASSIGN_WEIGHT:
-        class_weight = [np.array(Y_train).shape[0] / (np.array(Y_train) == i).sum() for i in range(len(set(Y_train)))]
-        class_weight = torch.FloatTensor(class_weight)
+    # num_batches = X_test.shape[0] // test_batch_size
+    # test_batch_generator = mask_batch_seq_generator(X_test, Y_test, masks_test, test_batch_size)
 
-    tokenizer = AutoTokenizer.from_pretrained(BERT_MODEL, normalization=True)
-    X_test, masks_test = tokenize_with_new_mask(X_test_raw, MAX_LENGTH, tokenizer)
+    # _, _, test_loss, test_auc, test_acc, test_tn, test_fp, test_fn, test_tp, test_precision, test_recall, test_s_pred = evaluate(model,
+    #                                                                                             test_batch_generator,
+    #                                                                                             num_batches, device,
+    #                                                                                             class_weight )
 
-    num_batches = X_test.shape[0] // test_batch_size
-    test_batch_generator = mask_batch_seq_generator(X_test, Y_test, masks_test, test_batch_size)
-
-    _, _, test_loss, test_auc, test_acc, test_tn, test_fp, test_fn, test_tp, test_precision, test_recall, test_s_pred = evaluate(model,
-                                                                                                test_batch_generator,
-                                                                                                num_batches, device,
-                                                                                                class_weight )
-
-    content = f'Test Acc: {test_acc * 100:.2f}%, AUC: {test_auc * 100:.2f}%, TN: {test_tn}, FP: {test_fp}, FN: {test_fn}, TP: {test_tp}, Precision: {test_precision* 100:.2f}%, Recall: {test_recall* 100:.2f}%'
-    print(content)
+    # content = f'Test Acc: {test_acc * 100:.2f}%, AUC: {test_auc * 100:.2f}%, TN: {test_tn}, FP: {test_fp}, FN: {test_fn}, TP: {test_tp}, Precision: {test_precision* 100:.2f}%, Recall: {test_recall* 100:.2f}%'
+    # print(content)
 
 if __name__ == '__main__':
     main()
