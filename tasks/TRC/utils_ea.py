216bc60da1c510dba5cc4f1089ffdf178a0a8725
auro736 <auroragensale@gmail.com>
2023-06-08 08:10:00 +0000

    utils for error analysis created

---
+++ tasks/TRC/utils_ea.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+from transformers import AutoConfig
+
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.calibration import CalibrationDisplay
+from sklearn.metrics import ConfusionMatrixDisplay
+
+from common_utils import extract_from_dataframe
+from TRC.models import ModelForWeightedSequenceClassification
+
+def load_local_model(model_path, config_path, device, model_name):
+
+    config = config = AutoConfig.from_pretrained(config_path)
+
+    model = ModelForWeightedSequenceClassification(model_name=model_name,config=config)
+    checkpoint = torch.load(model_path, map_location=device)
+    model.load_state_dict(checkpoint)
+
+    return model
+
+def create_weight(Y_train):
+    # weight of each class in loss function
+    class_weight = [np.array(Y_train).shape[0] / (np.array(Y_train) == i).sum() for i in range(len(set(Y_train)))]
+    class_weight = torch.FloatTensor(class_weight)
+    return class_weight
+
+def prepare_data(data_path, need_columns):
+
+    data = pd.read_pickle(data_path)
+    list = extract_from_dataframe(data, need_columns)
+    return list
+
+def calibration_plot(probabilities, y_true, img_path, model_name):
+    # probabilities = probabilità per classe
+    # y = batch ground truth
+
+    # per creare calibration plot from_predictions la doc dice che vuole (y_true, prob)
+    # dove prob sono le prob della classe positiva, ossia in probabilities
+    # gli el sono liste [P(0), P(1)] devo prendermi solo P(1)
+
+    pos_probs = probabilities[:,1] 
+    
+    disp = CalibrationDisplay.from_predictions(y_true, pos_probs, pos_label=1)
+    plt.title(model_name)
+    plt.savefig(img_path)
+
+def confusion_matrix(probabilities, y_true, model_name, path):
+
+    y_values, indices = torch.max(probabilities, 1)
+    # dato che la posizione nella lista di 2 elementi della probabilità 
+    # rappresenta la prob della label
+    y_pred = indices
+    conf_mat = ConfusionMatrixDisplay.from_predictions(y_true, y_pred, labels=[0, 1], colorbar=False)
+    plt.title(model_name)
+    plt.savefig(path)
--- tasks/error_analysis_TRC.py
+++ tasks/error_analysis_TRC.py
@@ -1,70 +1,19 @@
 import torch
 import torch.nn as nn
-from transformers import AutoConfig, AutoTokenizer
+from transformers import AutoTokenizer
 
 import random
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.calibration import CalibrationDisplay
-from sklearn.metrics import ConfusionMatrixDisplay
 
 from TRC.utils import tokenize_with_new_mask, evaluate
-from common_utils import extract_from_dataframe, mask_batch_seq_generator
-from TRC.models import ModelForWeightedSequenceClassification
+from TRC.utils_ea import *
+from common_utils import mask_batch_seq_generator
 
 # TRC 
 
 MAX_LENGTH = 128
 ASSIGN_WEIGHT = True
 SEED = 42
-
-
-def load_local_model(model_path, config_path, device, model_name):
-
-    config = config = AutoConfig.from_pretrained(config_path)
-
-    model = ModelForWeightedSequenceClassification(model_name=model_name,config=config)
-    checkpoint = torch.load(model_path, map_location=device)
-    model.load_state_dict(checkpoint)
-
-    return model
-
-def prepare_data(data_path, need_columns):
-
-    data = pd.read_pickle(data_path)
-    list = extract_from_dataframe(data, need_columns)
-    return list
-
-def create_weight(Y_train):
-    # weight of each class in loss function
-    class_weight = [np.array(Y_train).shape[0] / (np.array(Y_train) == i).sum() for i in range(len(set(Y_train)))]
-    class_weight = torch.FloatTensor(class_weight)
-    return class_weight
-
-def calibration_plot(probabilities, y_true, img_path, model_name):
-    # probabilities = probabilità per classe
-    # y = batch ground truth
-
-    # per creare calibration plot from_predictions la doc dice che vuole (y_true, prob)
-    # dove prob sono le prob della classe positiva, ossia in probabilities
-    # gli el sono liste [P(0), P(1)] devo prendermi solo P(1)
-
-    pos_probs = probabilities[:,1] 
-    
-    disp = CalibrationDisplay.from_predictions(y_true, pos_probs, pos_label=1)
-    plt.title(model_name)
-    plt.savefig(img_path)
-
-def confusion_matrix(probabilities, y_true, model_name, path):
-
-    y_values, indices = torch.max(probabilities, 1)
-    # dato che la posizione nella lista di 2 elementi della probabilità 
-    # rappresenta la prob della label
-    y_pred = indices
-    conf_mat = ConfusionMatrixDisplay.from_predictions(y_true, y_pred, labels=[0, 1], colorbar=False)
-    plt.title(model_name)
-    plt.savefig(path)
 
 
 def main():
